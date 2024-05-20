fdb5cc3f9a2504353f53f431b5515faa05670c32
Pedro Passos <passosm.pedroh@gmail.com>
2024-05-20 09:27:01 -0300

    projeto carro

---
+++ netbeans/Projeto_Carro/src/projeto_carro/Carro.java
@@ -0,0 +1,10 @@
+package projeto_carro;
+
+public class Carro {
+    public int Rodas = 4;
+    public int Portas = 4;
+    public String Nome;
+    public String Marca;
+    
+    
+}
---
+++ netbeans/Projeto_Carro/src/projeto_carro/Projeto_Carro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projeto_carro;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Projeto_Carro {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        Carro carro = new Carro();
+
+        carro.setRodas(4);
+
+        System.out.println(carro.quantasRodas());
+    }
+    
+}
