2d951d9b984a320ad9c4c7970a3da8f691986884
pedro-hpm <168486179+pedro-hpm@users.noreply.github.com>
2024-05-21 21:09:03 -0300

    finalização projeto_carro

--- netbeans/Projeto_Carro/src/projeto_carro/Carro.java
+++ netbeans/Projeto_Carro/src/projeto_carro/Carro.java
@@ -1,36 +1,76 @@
 package projeto_carro;
 
 public class Carro {
+    public String Modelo;
+    public String Marca;
+    public String Data_fabricacao;
+    public String Cor;
     public int Rodas;
     public int Portas;
-    public String Nome;
-    public String Marca;
-    public double Vel;
-    public boolean Buzina;
+    public int Capacidade_passageiros;
+    // public double Quilometragem;
+    public double Velocidade;
 
-    public Carro(String nome, String marca, double vel, String buzina) {
+    public Carro(String modelo, String marca, String data_fabricacao, String cor, int capacidade_passageiros, double vel) {
+        Modelo = modelo;
+        Marca = marca;
+        Data_fabricacao = data_fabricacao;
+        Cor = cor;
+        Capacidade_passageiros = capacidade_passageiros;
+        Velocidade = vel;
         Rodas = 4;
         Portas = 4;
-        Nome = nome;
-        Marca = marca;
-        Vel = vel;
-        if (buzina == "sim") {
-            Buzina = true;
-        }
     }
 
     public double Acelerar(double vel) {
         //System.out.println(Vel);
 
-        Vel += vel;
+        Velocidade += vel;
 
-        return Vel;
+        return Velocidade;
     }
 
     public double Freiar(double vel) {
-        Vel -= vel;
+        Velocidade -= vel;
 
-        return Vel;
+        return Velocidade;
+    }
+
+    public String TrocaMarcha() {
+        // + 10 km/h a cada marcha (inicial 20)
+        if (Velocidade > 0) {
+            return "Primeira marcha";
+        } 
+        else if (Velocidade >= 20) {
+            return "Segunda marcha";
+        } 
+        else if (Velocidade >= 50) {
+            return "Terceira marcha";
+        } 
+        else if (Velocidade >= 80) {
+            return "Quarta marcha";
+        } 
+        else if (Velocidade >= 120) {
+            return "Quinta marcha";
+        } 
+        else {
+            return "Neutro";
+        }
+    }
+
+    public String Re() {
+        if (TrocaMarcha() != "Neutro") {
+            return "Seu carro nao pode dar ré, porque seu carro esta em movimento";
+        }
+        else {
+            return "Dando ré..."; 
+        }
+    }
+
+    public void Buzinar(String buzina) {
+        if (buzina == "sim") {
+            System.out.println("Buzinando...");
+        }
     }
 }
 
--- netbeans/Projeto_Carro/src/projeto_carro/Projeto_Carro.java
+++ netbeans/Projeto_Carro/src/projeto_carro/Projeto_Carro.java
@@ -4,24 +4,40 @@ import java.util.*;
 
 public class Projeto_Carro {
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
 
-        String nome = scan.nextLine();
+        System.out.println("Entre com os dados do carro: ");
+        
+        System.out.print("Marca: ");
         String marca = scan.nextLine();
-        double vel = scan.nextDouble();
-        String buzina = scan.nextLine();
+        
+        System.out.print("Modelo: ");
+        String modelo = scan.nextLine();
+        
+        System.out.print("Data de fabricação: ");
+        String fabricacao = scan.nextLine();
+        
+        System.out.print("Cor: ");
+        String cor = scan.nextLine();
+        
+        System.out.println("Capacidade de passageiros: ");
+        int capacidade = scan.nextInt();
+        
+        System.out.print("Velocidade inicial: ");
+        double velocidade = scan.nextDouble();
 
-        Carro carro = new Carro(nome, marca, vel, buzina);
+        Carro carro = new Carro(modelo, marca, fabricacao, cor, capacidade, velocidade);
+
+        //...
+        System.out.print("trocar de  marcha ou manter a mesma? ");
+        //...
 
         System.out.println(carro);
         
         System.out.println(carro.Acelerar(20.0));
 
         System.out.println(carro.Freiar(10.0));
-
-        System.out.println(carro.Buzina);
 
         scan.close();
     }
